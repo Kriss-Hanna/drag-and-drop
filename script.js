5f58b0df9b867e26e55031689f43593eed9fa42f
Kriss-Hanna <kristofferson.hanna@gmail.com>
2021-06-10 18:12:03 +0200

    js correction

--- script.js
+++ script.js
@@ -15,3 +15,31 @@ function dragStart(){
 function dragEnd(){
   this.className = "base"
 };
+
+for(const empty of box){
+
+  empty.addEventListener("dragover", dragOver);
+  empty.addEventListener("dragenter", dragEnter);
+  empty.addEventListener("dragleave", dragLeave);
+  empty.addEventListener("drop", dragDrop);
+
+}
+
+function dragOver(e){
+  e.preventDefault();
+
+  console.log("over")
+};
+
+function dragEnter(){
+  this.className += " hovered"
+};
+
+function dragLeave(){
+  this.className = "case"
+};
+
+function dragDrop(){
+  this.className = "case"
+  this.append(base);
+};
